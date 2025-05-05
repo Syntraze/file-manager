ae3769dcbf959a243ca597534579a43d831c9ec3
Syntraze <syntraze@gmail.com>
2025-05-06 01:08:28 +0200

    feat:improve list

--- fs/list.js
+++ fs/list.js
@@ -1,14 +1,26 @@
-import fs from "fs";
+import fs from "fs/promises";
+import path from "path";
 
 export const list = async () => {
-  const currentDir = process.cwd();
-  return new Promise((resolve, reject) => {
-    fs.readdir(currentDir, (err, files) => {
-      if (err) {
-        reject("Operation failed");
-      } else {
-        resolve(files);
-      }
-    });
-  });
+  try {
+    const currentDir = process.cwd();
+    const files = await fs.readdir(currentDir);
+
+    const detailedList = await Promise.all(
+      files.map(async (file) => {
+        const filePath = path.join(currentDir, file);
+        const stats = await fs.stat(filePath);
+        return {
+          Name: file,
+          Type: stats.isDirectory() ? "directory" : "file",
+        };
+      })
+    );
+
+    detailedList.sort((a, b) => a.Name.localeCompare(b.Name));
+
+    console.table(detailedList);
+  } catch (err) {
+    console.log("Operation failed");
+  }
 };
